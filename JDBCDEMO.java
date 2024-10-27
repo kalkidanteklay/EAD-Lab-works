12619da23f426150ab70a970895ea14ff28c91fa
kalkidanteklay <150287564+kalkidanteklay@users.noreply.github.com>
2024-10-27 18:25:15 +0300

    Add files via upload

---
+++ JDBCDEMO.java
@@ -0,0 +1,27 @@
+package com.example.JDBC; 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCDEMO {
+	 public static void main(String[] args) {
+	        String url = "jdbc:mysql://localhost:3306/students";
+	        String user = "root"; 
+	        String password = "171995Iwasborn#"; 
+	        String databaseName = "new_database";
+
+	        try {
+	            Connection connection = DriverManager.getConnection(url, user, password);
+	            Statement statement = connection.createStatement();	 
+	            String createDatabaseSQL = "CREATE DATABASE " + databaseName;
+	            statement.executeUpdate(createDatabaseSQL);
+	            System.out.println("Database " + databaseName + "created successfully.");
+	            statement.close();
+	           connection.close(); 
+	        } catch (SQLException e) {
+	            System.err.println("Connection failed: " + e.getMessage());
+	        }
+	    }
+
+}
